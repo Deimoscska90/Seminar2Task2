1aa9e6b88ca8ee32ad5e0182c6e5920f6a0f16b1
Filipp <Filipp_nikolaev@rambler.ru>
2023-01-31 14:02:52 +0600

    Seminar2Task2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int GetNumber()
+{
+    Random rnd = new Random();
+    int result = rnd.Next(1, 1000);
+    return result;
+}
+
+int FindNumber(int number)
+{   
+int ThirdNumber = number%10; 
+
+return ThirdNumber;
+}
+
+int number = GetNumber();
+int result = FindNumber(number);
+
+
+Console.WriteLine($"{number}");
+
+if (number/100 == 0)
+{
+    Console.WriteLine("Нет числа");
+}
+else
+{
+    Console.WriteLine($"{result}");
+}
